1fcc69e5ac5eaa77af72f02cf0039c8edfb231e6
brabii <collectorsgarbages@gmail.com>
2021-02-28 00:45:21 +0100

    initial commit

---
+++ src/main/java/com/scrabble/App.java
@@ -0,0 +1,13 @@
+package com.scrabble;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
